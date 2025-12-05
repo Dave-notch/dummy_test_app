91200e2f7f34892e51120b8b3e251fd545103ecb
Dave-notch <stsaamir86@gmail.com>
2025-12-05 22:19:14 +0300

    drawer and bottom navigations

--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -29,7 +29,14 @@ class _firstpageState extends State<firstpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('first page'),
+      appBar: AppBar(title: Text(
+        'A\'nor',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w500
+        ),
+      
+      ),
       backgroundColor: Colors.blue,),
       body: _pages[selectedindex],
       bottomNavigationBar: BottomNavigationBar(
@@ -54,6 +61,77 @@ class _firstpageState extends State<firstpage> {
       
      ]
      ),
+     drawer: Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 19, 105, 88), // background for drawer header
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "𝔸𝕩𝕚𝕤",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                // YOUTUBE STYLE SUBSCRIBE BUTTON
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.red, // YouTube red
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.login, color: Colors.white, size: 20),
+                      SizedBox(width: 8),
+                      Text(
+                        "Sign In",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ListTile(
+          leading: Icon(Icons.privacy_tip),
+          title: Text(
+            'P R I V A C Y',
+            style: TextStyle(
+              // fontWeight: FontWeight.w700,
+            ),
+            ),
+        ),
+
+        ListTile(
+          leading: Icon(Icons.logout),
+          title: Text('L O G O U T',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+          ),
+        )
+          
+        ],
+      ),
+    )
+
     );
   }
 }
